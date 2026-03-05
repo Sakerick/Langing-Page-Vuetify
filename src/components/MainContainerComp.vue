64aeff7a6ed3e44d3bf5c43adde4250028230ff2
Sakerick <161174620+Sakerick@users.noreply.github.com>
2026-03-05 00:30:24 -0800

    Refactor MainContainerComp to improve image loading

--- src/components/MainContainerComp.vue
+++ src/components/MainContainerComp.vue
@@ -1,18 +1,49 @@
 <template>
   <v-container class="d-flex flex-column align-center justify-center">
-    <v-row>
+    <v-row class="w-100 pa-5">
       <v-col cols="6">
-        <CardComp :blur="false" :src="'https://picsum.photos/600/600/?random=' + Math.floor(Math.random() * 1000)" text="Al cliquear la imagen se generará una nueva imagen aleatoria" />
-      </v-col>
-      <v-col cols="6">
+        <v-skeleton-loader
+          v-if="isLoading"
+          height="400"
+          type="card, list-item-two-line"
+        />
         <CardComp
-          :blur="true"
-          :src="'https://picsum.photos/600/600/?blur&random=' + Math.floor(Math.random() * 1000)"
-          text="Al cliquear la imagen se generará una nueva imagen con efecto blur aleatoria"
+          v-else
+          :author="author1"
+          description="Con botón se generará una nueva imagen aleatoria"
+          :src="card1Src"
+          title="Titulo card 1"
+        />
+      </v-col>
+
+      <v-col cols="6">
+        <v-skeleton-loader
+          v-if="isLoading"
+          height="400"
+          type="card, list-item-two-line"
+        />
+
+        <CardComp
+          v-else
+          :author="author2"
+          description="Con botón se generará una nueva imagen aleatoria"
+          :src="card2Src"
+          title="Titulo card 2"
         />
       </v-col>
     </v-row>
-    <v-row>
+
+    <v-btn
+      class="pa-5 ma-5"
+      color="primary"
+      :disabled="isLoading"
+      :loading="isLoading"
+      @click="changeImages()"
+    >
+      Cambiar imagenes
+    </v-btn>
+
+    <v-row class="w-100">
       <v-col cols="12">
         <TableComp />
       </v-col>
@@ -21,6 +52,50 @@
 </template>
 
 <script lang="ts" setup>
+  import { onMounted, ref } from 'vue'
   import CardComp from './CardComp.vue'
   import TableComp from './TableComp.vue'
+
+  const card1Src = ref('')
+  const card2Src = ref('')
+  const author1 = ref('')
+  const author2 = ref('')
+  const isLoading = ref(false)
+
+  async function changeImages () {
+    isLoading.value = true
+    try {
+      const [img1, img2] = await fetchRandomImages()
+      // Asignamos los valores
+      card1Src.value = img1.url
+      author1.value = img1.author
+      card2Src.value = img2.url
+      author2.value = img2.author
+    } catch (error) {
+      console.error(error)
+    } finally {
+      // Pequeño timeout opcional si la API es demasiado rápida
+      // y quieres que el efecto de carga se note.
+      setTimeout(() => {
+        isLoading.value = false
+      }, 500)
+    }
+  }
+
+  async function fetchRandomImages () {
+    const response = await fetch('https://picsum.photos/v2/list?limit=30')
+    const data = await response.json()
+    const idx1 = Math.floor(Math.random() * data.length)
+    let idx2 = Math.floor(Math.random() * data.length)
+    while (idx1 === idx2) idx2 = Math.floor(Math.random() * data.length)
+
+    return [
+      { url: `https://picsum.photos/id/${data[idx1].id}/400/400`, author: data[idx1].author },
+      { url: `https://picsum.photos/id/${data[idx2].id}/400/400`, author: data[idx2].author },
+    ]
+  }
+
+  onMounted(() => {
+    changeImages()
+  })
 </script>
