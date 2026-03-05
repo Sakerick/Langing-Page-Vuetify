6bf12c3b2be62df8888b1ae3c808b59d3042908c
Sakerick <161174620+Sakerick@users.noreply.github.com>
2026-03-05 00:30:11 -0800

    Update HeaderComp.vue

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,12 +1,13 @@
 <template>
-  <v-card class="d-flex text-center" dark>
-    <v-img
-      color="surface-variant"
-      cover
-      height="200"
-      src="https://picsum.photos/id/135/600/200"
-    >
-      <v-toolbar-title class="text-display-medium pa-15 text-white">Mi página web con vuetify</v-toolbar-title>
-    </v-img>
-  </v-card>
+  <v-app-bar>
+    <v-avatar>
+      <v-img
+        color="surface-variant"
+        cover
+        height="200"
+        src="https://picsum.photos/id/135/600/200"
+      />
+    </v-avatar>
+    <v-toolbar-title class="text-display-medium text-center pa-15 text-white">Mi página web con vuetify</v-toolbar-title>
+  </v-app-bar>
 </template>
