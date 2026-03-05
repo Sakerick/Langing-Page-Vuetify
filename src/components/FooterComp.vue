e17cbe1b5f3d4dcbdef9a08331e18af80e363ea8
Sakerick <161174620+Sakerick@users.noreply.github.com>
2026-03-05 00:29:54 -0800

    Update FooterComp.vue

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,5 +1,32 @@
 <template>
   <v-footer class="d-flex text-center pa-4" dark>
-    <v-toolbar-title>Todos los derechos reservados.</v-toolbar-title>
+    <v-row>
+      <v-col>
+        Herick Gerardo Vizcarra Macias
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        1190154
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        {{ obtenerFecha() }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        Correo de contacto: herick.vizcarra@uabc.edu.mx
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
+
+<script lang="ts" setup>
+  function obtenerFecha () {
+    const fecha = new Date()
+    return fecha.toLocaleDateString()
+  }
+
+</script>
